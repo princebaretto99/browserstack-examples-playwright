1a7e969de8e7208949ed8522819bcf820aa2ecac
princebaretto99 <princebaretto@gmail.com>
2022-01-11 22:29:19 +0530

    Simple Login

---
+++ resources/conf/playwright.config.js
@@ -0,0 +1,24 @@
+const { devices } = require('@playwright/test');
+
+const config = {
+  use: {
+    trace: 'on-first-retry',
+  },
+  testDir: '../../src',
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+  ],
+};
+
+module.exports = config;
