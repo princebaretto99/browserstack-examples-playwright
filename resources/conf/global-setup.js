efe8a69a04948983dfb11e0488e2456b602235fd
princebaretto99 <princebaretto@gmail.com>
2022-02-22 18:11:18 +0530

    Fixed Local

--- resources/conf/global-setup.js
+++ resources/conf/global-setup.js
@@ -1,21 +1,6 @@
 const { bsLocal, BS_LOCAL_ARGS } = require('../../fixtures');
-const { promisify } = require('util');
-const sleep = promisify(setTimeout);
 
 module.exports = async () => {
     console.log("Setting Up Local Connection...");
-    let localResponseReceived = false;
-    bsLocal.start(BS_LOCAL_ARGS, (err)=>{
-        if(err) {
-            console.log(err);
-        }else{
-            console.log("BrowserStack Local is Started");
-        }
-        localResponseReceived = true;
-    })
-    while (!localResponseReceived) {
-        await sleep(1000);
-        console.log("Waiting..");
-      }
-  };
-  
+    await new Promise(r => bsLocal.start(BS_LOCAL_ARGS, r));
+};
--- resources/conf/global-teardown.js
+++ resources/conf/global-teardown.js
@@ -3,16 +3,9 @@ const { promisify } = require('util');
 const sleep = promisify(setTimeout);
 
 module.exports = async () => {
-    let localStopped = false;
 
     if (bsLocal && bsLocal.isRunning()) {
-        bsLocal.stop(() => {
-            localStopped = true;
-            console.log('Stopped BrowserStackLocal');
-        });
-        while (!localStopped) {
-            await sleep(1000);
-          }
+        await new Promise(r => bsLocal.stop(r))
     }
-    
-}
+
+};
